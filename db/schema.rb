cc04544c256f7bb3a3b5e2a4bca7f91fe6629d22
santiago solana <santiago.sjs@gmail.com>
2016-12-27 03:01:32 +0000

    added username to doodles

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161227012637) do
+ActiveRecord::Schema.define(version: 20161227013412) do
+
+  create_table "doodles", force: :cascade do |t|
+    t.string   "canvas"
+    t.string   "title"
+    t.integer  "rating"
+    t.integer  "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
