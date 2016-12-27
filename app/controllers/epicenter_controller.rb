65fa125f83e66b472a65f7023814f04708134b14
Bri <brianna.k.meadows95@gmail.com>
2016-12-27 05:24:33 +0000

    added following

--- app/controllers/epicenter_controller.rb
+++ app/controllers/epicenter_controller.rb
@@ -1,7 +1,13 @@
 class EpicenterController < ApplicationController
   def feed
+    @following_doodles = []
+    
+    Doodle.all.each do |doodle|
+      if current_user.following.include?(doodle.user_id)
+        @following_doodles.push(doodle)
+      end
+    end
   end
-
   def trending
   end
 
@@ -9,6 +15,7 @@ class EpicenterController < ApplicationController
   end
 
   def now_following
+    @user = User.find(params[:id].to_i)
   end
 
   def unfollow
