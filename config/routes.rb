d3b88ffbbea23d57d3610c7cde81d051047c8c8a
Bri <brianna.k.meadows95@gmail.com>
2016-12-28 23:28:12 +0000

    routes changed

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-
   root 'epicenter#feed'
 
   get 'trending' => 'epicenter#trending'
@@ -12,7 +11,6 @@ Rails.application.routes.draw do
 
   resources :doodles
   
-
   devise_for :users
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
